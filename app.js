1ac13e0e8f4b6cc45c3adef3d676b77f871b745f
minydev <108986809+minydev@users.noreply.github.com>
2022-07-22 15:38:54 +0900

    Moved 'fortune cookie' into module.

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
+
+const fortune = require('./lib/fortune')
+
 const app = express()
-const port = process.env.PORT || 3000
 
 // 핸들바 뷰 엔진 설정
 app.engine('handlebars', expressHandlebars({
@@ -9,13 +11,14 @@ app.engine('handlebars', expressHandlebars({
 }))
 app.set('view engine', 'handlebars')
 
+const port = process.env.PORT || 3000
+
 app.use(express.static(__dirname + '/public'))
 
 // app.get: 라우트 추가 메서드
 app.get('/', (req, res) => res.render('home'))
 app.get('/about', (req, res) => {
-    const randomFortune = fortunes[Math.floor(Math.random() * fortunes.length)]
-    res.render('about', { fortune: randomFortune })
+    res.render('about', { fortune: fortune.getFortune() })
 })
 
 // custom 404 page
@@ -30,14 +33,6 @@ app.use((err, req, res, next) => {
     res.status(500)
     res.render('500')
 })
-
-const fortunes = [
-    "인생이 행복해지는 비결은 끊임없이 작은선물을 받는 것이다.",
-    "평범하다는 것은 '현대판 노예'라는 뜻이다.",
-    "매일 1퍼센트의 차이가 3개월을 넘기면 100퍼센트의 차이를 만든다는 사실을 기억하라.",
-    "실패는 언제나 찾아오는 친구이며 성공은 어쩌다 찾아오는 손님이다.",
-    "그저 첫 발걸음을 떼면 됩니다. 계단 전체를 올려다볼 필요도 없습니다. 그저 첫 발걸음만 떼면 됩니다."
-]
 
 app.listen(port, () => console.log(
     `Express started on http://localhost:${port};` + `press Ctrl-C to terminate...`
