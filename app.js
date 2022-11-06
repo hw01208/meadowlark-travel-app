87ecef67dff011e534f5acf7ab0327fdbc17dab2
youngdev57 <hw01208@naver.com>
2022-11-06 23:44:35 +0900

    Make test code with jest.

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const handlers = require('./lib/handlers')
+
 const express = require('express')
 const expressHandlebars = require('express-handlebars')
 
@@ -16,23 +18,27 @@ const port = process.env.PORT || 3000
 app.use(express.static(__dirname + '/public'))
 
 // app.get: 라우트 추가 메서드
-app.get('/', (req, res) => res.render('home'))
-app.get('/about', (req, res) => {
-    res.render('about', { fortune: fortune.getFortune() })
-})
+// app.get('/', (req, res) => res.render('home'))
+app.get('/', handlers.home)
+// app.get('/about', (req, res) => {
+//     res.render('about', { fortune: fortune.getFortune() })
+// })
+app.get('/about', handlers.about)
 
 // custom 404 page
-app.use((req, res) => {
-    res.status(404)
-    res.render('404')
-})
+// app.use((req, res) => {
+//     res.status(404)
+//     res.render('404')
+// })
+app.use(handlers.notFound)
 
 // custom 500 page
-app.use((err, req, res, next) => {
-    console.log(err.message)
-    res.status(500)
-    res.render('500')
-})
+// app.use((err, req, res, next) => {
+//     console.log(err.message)
+//     res.status(500)
+//     res.render('500')
+// })
+app.use(handlers.serverError)
 
 app.listen(port, () => console.log(
     `Express started on http://localhost:${port};` + `press Ctrl-C to terminate...`
